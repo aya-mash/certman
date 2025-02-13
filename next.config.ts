4609eb8bb125a5d9a7b4a1589421efa971e5bfb2
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-02-13 20:40:02 +0200

    configuring headers using vecel config file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  crossOrigin: "anonymous", // or 'use-credentials'
+  // crossOrigin: "anonymous", // or 'use-credentials'
 };
 
 export default nextConfig;
