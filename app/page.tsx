96f7e0e7d788b1505385dd6047188daee8ac7dfd
AYABULELA YANELA MAHLATHINI <ayabulela_yanela.mahlathini@mercedes-benz.com>
2025-02-18 04:59:38 +0200

    work in progress

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,7 @@ import { Header } from "@/components/Header";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import { authOptions } from "@/lib/authSession";
+import IssueCertificateAutoForm from "@/components/IssueCertificateAutoForm";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -27,11 +28,16 @@ export default async function Home() {
           <h1 className="text-2xl font-bold mb-4">SSL Certificate Issuer</h1>
           <TabsList>
             <TabsTrigger value="certificates">Your Certificates</TabsTrigger>
+            <TabsTrigger value="auto">ACME Auto</TabsTrigger>
+
             <TabsTrigger value="client">ACME Client</TabsTrigger>
             <TabsTrigger value="sh">ACME.SH</TabsTrigger>
           </TabsList>
           <TabsContent value="certificates">
             <CertificatesList userId={session.user.id} />
+          </TabsContent>
+          <TabsContent value="auto">
+            <IssueCertificateAutoForm />
           </TabsContent>
           <TabsContent value="client">
             <IssueCertificateForm />
